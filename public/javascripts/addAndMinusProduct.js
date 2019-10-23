7ee15afd06a6ce7455296f75491e6a9d00e233cc
Anthony Michael Stephens <mike@ThE-MacBook-Pro-9.local>
2019-10-23 10:25:02 -0400

    Added pagination for products with working page number links

---
+++ public/javascripts/addAndMinusProduct.js
@@ -0,0 +1,19 @@
+let amount = parseInt($('#total').text())
+let price = parseFloat($('#priceValue').val())
+
+$("#minus").click(function () {
+    if (amount === 1) {
+        $('#total').text(amount)
+        $('#priceValue').val(price)
+        // return;
+    } else {
+        $('#total').text(--amount)
+        $('#priceValue').val((price * amount).toFixed(2))
+    }
+})
+$("#plus").click(function () {
+    $('#total').text(++amount)
+    $('#priceValue').val((price * amount).toFixed(2))
+
+})
+
